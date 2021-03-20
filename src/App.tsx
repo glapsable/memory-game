c9043b7002144fcd3b6fd1eeabf5a5bcc4b02f48
mariusz glapka <mariusz.glapka@4soft.co>
2021-03-20 18:36:34 +0100

    add linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   BrowserRouter,
 } from "react-router-dom";
-import Router from "./reoutes/Router";
+import Router from "./routes/Router";
 import {CssBaseline} from "@material-ui/core";
 import Layout from "./components/Layout/Layout";
 
